9266aa98c3ded9397c0a17590c6e02710a3bbf64
DilshanHarendra <harendracripto@gmail.com>
2022-11-25 14:25:49 +0530

    modify store

--- src/store/store.js
+++ src/store/store.js
@@ -6,13 +6,17 @@ import {configureStore} from '@reduxjs/toolkit';
 const persistConfig = {
   key: 'v1',
   storage: AsyncStorage,
-  whitelist: ['products'],
+  whitelist: [],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
