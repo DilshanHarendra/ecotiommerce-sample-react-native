e64b0837bb5e6422394a74602fd538b9c6853ef1
DilshanHarendra <harendracripto@gmail.com>
2022-11-25 14:23:07 +0530

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const {PRIMARY_COLOR} = require('./src/shared/constant');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: PRIMARY_COLOR,
+      },
+    },
   },
   plugins: [],
-}
+};
