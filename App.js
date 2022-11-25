06eb2d22f456ee625a348b2f8304ce296c95976a
DilshanHarendra <harendracripto@gmail.com>
2022-11-25 14:26:11 +0530

    connect tab view

--- App.js
+++ App.js
@@ -8,20 +8,17 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {persistor, store} from './src/store/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import Splash from './src/screens/splash/Splash';
-import SafeAreaView from 'react-native/Libraries/Components/SafeAreaView/SafeAreaView';
+import Router from './src/router/Router';
 
 const App: () => Node = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={<Splash />} persistor={persistor}>
-        <SafeAreaView>
-          <Text>Hi</Text>
-        </SafeAreaView>
+        <Router />
       </PersistGate>
     </Provider>
   );
